4b57ea84b336de9b9d3ef8f78e19585fd67c3b6a
Romain MEUNIER <romain.meunier@safrangroup.com>
2026-03-24 17:55:21 +0100

    Narrow fusion feather at borders

--- src/stitching/editable/optimized_stitching_algo.py
+++ src/stitching/editable/optimized_stitching_algo.py
@@ -9,7 +9,7 @@ from stitching.contracts import ReconstructionSurface, ScenarioConfig, SubApertu
 
 # Configuration anti-artefacts
 EDGE_EROSION_PX = 1
-FEATHER_WIDTH = 0.20
+FEATHER_WIDTH = 0.15
 SOLVE_FEATHER_WIDTH = 0.45
 
 class CandidateStitcher:
