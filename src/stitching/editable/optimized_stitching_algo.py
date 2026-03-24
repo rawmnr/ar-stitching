4170a57de1681f55c3e546b40ec6306b6d8ae7f6
Romain MEUNIER <romain.meunier@safrangroup.com>
2026-03-24 17:45:55 +0100

    Tune solve feather taper narrower

--- src/stitching/editable/optimized_stitching_algo.py
+++ src/stitching/editable/optimized_stitching_algo.py
@@ -10,7 +10,7 @@ from stitching.contracts import ReconstructionSurface, ScenarioConfig, SubApertu
 # Configuration anti-artefacts
 EDGE_EROSION_PX = 2
 FEATHER_WIDTH = 0.20
-SOLVE_FEATHER_WIDTH = 0.35
+SOLVE_FEATHER_WIDTH = 0.30
 
 class CandidateStitcher:
     def reconstruct(
