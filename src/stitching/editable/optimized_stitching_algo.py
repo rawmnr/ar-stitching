935a9fabdf48e378d94e211302bc9611f392a44d
Romain MEUNIER <romain.meunier@safrangroup.com>
2026-03-24 18:01:11 +0100

    Revert "Combine tighter solve and fusion border weights"
    This reverts commit bc970fd16708257df6b9acaf3f6e402c082bfee6.

--- src/stitching/editable/optimized_stitching_algo.py
+++ src/stitching/editable/optimized_stitching_algo.py
@@ -10,7 +10,7 @@ from stitching.contracts import ReconstructionSurface, ScenarioConfig, SubApertu
 # Configuration anti-artefacts
 EDGE_EROSION_PX = 1
 FEATHER_WIDTH = 0.05
-SOLVE_FEATHER_WIDTH = 0.50
+SOLVE_FEATHER_WIDTH = 0.45
 
 class CandidateStitcher:
     def reconstruct(
