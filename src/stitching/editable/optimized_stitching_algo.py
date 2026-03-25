3916e6da037814b167d32d05a69c5e19041aa40a
Romain MEUNIER <romain.meunier@safrangroup.com>
2026-03-25 11:45:06 +0100

    SOLVE_FEATHER=0.50: new best RMS 1.10582448

--- src/stitching/editable/optimized_stitching_algo.py
+++ src/stitching/editable/optimized_stitching_algo.py
@@ -9,7 +9,7 @@ from stitching.contracts import ReconstructionSurface, ScenarioConfig, SubApertu
 
 EDGE_EROSION_PX = 1
 FEATHER_WIDTH = 0.05
-SOLVE_FEATHER_WIDTH = 0.495
+SOLVE_FEATHER_WIDTH = 0.50
 sigma_filter = 1.346
 n_irls = 1
 n_siac = 5
